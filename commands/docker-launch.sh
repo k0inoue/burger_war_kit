9a616441c9c3074d2989e88a79339986b8ea11e1
Kotaro Inoue <k.inoue@effect-effect.com>
2021-02-15 14:07:50 +0900

    テスト確認中

--- commands/docker-launch.sh
+++ commands/docker-launch.sh
@@ -81,6 +81,7 @@ fi
 
 # 新たにコンテナを起動する
 #------------------------------------------------
+#  --mount type=bind,src=${HOST_WS_DIR},dst=${CONTAINER_WS_DIR} \
 set -x
 docker run \
   --name ${KIT_DOCKER_CONTAINER_NAME} \
@@ -88,7 +89,6 @@ docker run \
   --privileged \
   --net host \
   --mount type=bind,src=/tmp/.X11-unix/,dst=/tmp/.X11-unix \
-  --mount type=bind,src=${HOST_WS_DIR},dst=${CONTAINER_WS_DIR} \
   --device /dev/snd \
   -v /dev/shm \
   -e DISPLAY=${DISPLAY} \
