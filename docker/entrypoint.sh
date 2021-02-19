d4bd31c0536c179ae1b336b5066a2ea7192f6e73
Kotaro Inoue <k.inoue@effect-effect.com>
2021-02-19 16:06:18 +0900

    gokisyusei

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,17 +4,17 @@ set -e
 # DEVELOPER_NAMEはdocker build時に置き換える
 DEVELOPER_NAME=developer
 
+echo "HOST_USER_ID: ${HOST_USER_ID}"
+echo "HOST_GROUP_ID: ${HOST_GROUP_ID}"
+echo "RUN USER ID: $(id)"
+
 if [ "$(id -u)" == "0" ]; then
 
-  echo "HOST_USER_ID: ${HOST_USER_ID}"
-  echo "HOST_GROUP_ID: ${HOST_GROUP_ID}"
-
-  echo "RUN USER ID: $(id)"
   echo "DEVELOPER ID: $(gosu ${DEVELOPER_NAME} id)"
 
   if [ "${HOST_USER_ID}" != "$(gosu ${DEVELOPER_NAME} id -u)" ]; then
     # ホストPCとUSER ID/GROUP IDを合わせる(ファイルアクセスできなくなる為)
-    usermod -u ${HOST_USER_ID} -m -d /home/${DEVELOPER_NAME} ${DEVELOPER_NAME}
+    usermod -u ${HOST_USER_ID} -o -m -d /home/${DEVELOPER_NAME} ${DEVELOPER_NAME}
     groupmod -g ${HOST_GROUP_ID} ${DEVELOPER_NAME}
     chown -R ${DEVELOPER_NAME}:${DEVELOPER_NAME} /home/${DEVELOPER_NAME}
   fi
