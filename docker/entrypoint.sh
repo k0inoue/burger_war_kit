de16e7589f95521cedeb5dcb0adaacee682ef3cd
Kotaro Inoue <k.inoue@effect-effect.com>
2021-02-19 14:00:09 +0900

    update

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -16,7 +16,7 @@ if [ "$(id -u)" == "0" ]; then
     # ホストPCとUSER ID/GROUP IDを合わせる(ファイルアクセスできなくなる為)
     usermod -u ${HOST_USER_ID} -o -m -d /home/${DEVELOPER_NAME} ${DEVELOPER_NAME}
     groupmod -g ${HOST_GROUP_ID} ${DEVELOPER_NAME}
-    chown -R ${HOST_USER_ID}:${HOST_GROUP_ID} /home/${DEVELOPER_NAME} /home/${DEVELOPER_NAME}/.*
+    chown -R ${HOST_USER_ID}:${HOST_GROUP_ID} /home/${DEVELOPER_NAME}
   fi
   echo "DEVELOPER ID: $(gosu ${DEVELOPER_NAME} id)"
 
